bdf44f35bbc734807b763c49f85d6fef7c929129
Ryan Wold <wold@afomi.com>
2025-01-13 19:52:37 -0800

    an empty cart

--- test/qart_web/controllers/page_controller_test.exs
+++ test/qart_web/controllers/page_controller_test.exs
@@ -3,6 +3,8 @@ defmodule QartWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+    assert html_response(conn, 200) =~ "open-source software"
+    assert html_response(conn, 200) =~ "qart.app"
+    assert html_response(conn, 200) =~ "peer to peer commerce online"
   end
 end
