f3fb17d148fa3865c8851cc5fd31e09f23638564
Ryan Wold <wold@afomi.com>
2025-02-02 17:37:12 -0800

    handle an Item's tags

--- lib/qart/inventory/item.ex
+++ lib/qart/inventory/item.ex
@@ -15,8 +15,23 @@ defmodule Qart.Inventory.Item do
 
   @doc false
   def changeset(item, attrs) do
+    attrs = normalize_tags(attrs)
+
     item
     |> cast(attrs, [:name, :description, :price, :tags, :status])
     |> validate_required([:name, :description])
   end
+
+  defp normalize_tags(%{"tags" => tags} = attrs) when is_binary(tags) do
+    tags_list =
+      tags
+      |> String.split(",") # Split by comma
+      |> Enum.map(&String.trim/1) # Trim whitespace
+      |> Enum.reject(&(&1 == "")) # Remove empty entries
+
+    Map.put(attrs, "tags", tags_list)
+  end
+
+  # Return unchanged if not a binary
+  defp normalize_tags(attrs), do: attrs
 end
