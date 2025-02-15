56008a4d5de38a410e307944c20fa1ab05328a69
Ryan Wold <wold@afomi.com>
2025-02-14 22:42:32 -0800

    social preview

--- lib/qart_web/controllers/page_controller.ex
+++ lib/qart_web/controllers/page_controller.ex
@@ -4,10 +4,17 @@ defmodule QartWeb.PageController do
   def home(conn, _params) do
     # The home page is often custom made,
     # so skip the default app layout.
-    render(conn, :home, layout: false)
+    render(conn, :home,
+      page_title: "Home",
+      page_description: "Peer to peer commerce",
+      layout: false
+    )
   end
 
   def start(conn, _params) do
-    render(conn, :start)
+    render(conn, :start,
+      items: Qart.Inventory.list_items(),
+      page_title: "Start"
+    )
   end
 end
