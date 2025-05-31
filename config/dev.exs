11d64532de802318aaf687cc49bdffe6b974b544
pingocreativetechnology <92856771+pingocreativetechnology@users.noreply.github.com>
2025-05-31 14:25:25 +1000

    Updated dev.exs w/ nopg just sqlite

--- config/dev.exs
+++ config/dev.exs
@@ -1,14 +1,21 @@
 import Config
 
 # Configure your database
-config :qart, Qart.Repo,
-  username: "postgres",
-  password: "postgres",
-  hostname: "localhost",
-  database: "qart_dev",
+# config :qart, Qart.Repo,
+#  username: "postgres",
+#  password: "postgres",
+#  hostname: "localhost",
+#  database: "qart_dev",
+#  stacktrace: true,
+#  show_sensitive_data_on_connection_error: true,
+#  pool_size: 10
+
+# Configure your database for SQLite
+config :your_app, YourApp.Repo,
+  database: Path.expand("../your_app_dev.db", Path.dirname(__ENV__.file)),
+  pool_size: 5,
   stacktrace: true,
-  show_sensitive_data_on_connection_error: true,
-  pool_size: 10
+  show_sensitive_data_on_connection_error: true
 
 # For development, we disable any cache and enable
 # debugging and code reloading.
